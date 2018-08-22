df0d25b41876464435bfaf56e0ee55a324994d3e
Sven Berglund <sven.berglund@innovatesecurity.se>
2018-08-22 14:44:14 -0400

    Implementing a simple kinda continuous and still pseudo random algorithm for the state generation

--- src/models/asyncAPIWrapper.jsx
+++ src/models/asyncAPIWrapper.jsx
@@ -9,25 +9,60 @@ class AsyncAPIWrapper{
     constructor(){
         this.publishing = false;
         this.channels = null;
+        this.channelState = new Map(); // continue here - make the state continuos!
+        this.channelDirection = new Map();
     }
+
 
     startPublishing(channels){
 
         this.publishing = true;
-        //console.log(`Starting to publish`);
+        this.channels = channels;
+        let channelState = new Map();
+        let channelDirection = new Map();
+
+        channels.forEach(function (channel_) {
+            let inputState = randomInt(0,1000);
+            channelState.set(channel_, inputState );
+            channelDirection.set(channel_ , inputState>500 ? false : true );
+        });
 
         let delay = 1000;
         let timerId = setTimeout(function request() {
 
-        channels.forEach(function (channel_){        
-                pubsub.publish(`asyncTask/${channel_}/state`, [randomInt(0,1000)]);
-        });
-        if (1 <= 0) {
-                // increase the interval to the next run - flexbility, 
-                // we can use this to decrease or increase delay if there are many tasks ..
-                delay *= 2;
-         }
-        timerId = setTimeout(request, delay);
+            channels.forEach(function (channel_) {
+                let offset = randomInt(0,10);
+                let oldState = channelState.get(channel_);
+                let oldDirection = channelDirection.get(channel_);
+
+                let newState = oldDirection ? oldState + offset : oldState - offset;
+            
+                // pushing back the new state if its outside boundaries
+                if ( newState <0 ) {
+                    newState = offset;
+                }else if (newState > 1000){
+                    newState = 1000 - offset;
+                };
+                
+                channelState.set(channel_, newState);
+                pubsub.publish(`asyncTask/${channel_}/state`, [channelState.get(channel_)]);
+
+                // change direction?
+                if (randomInt(0, 15) === 1) {
+                    channelDirection.set(channel_, !channelDirection.get(channel_));;
+                }
+                // change the delay?
+                let delayDeterminator = randomInt(0, 5);
+                if (delayDeterminator === 1) {
+                    delay = 3000;
+                } else {
+                    delay = 1000;
+                }
+
+            });
+
+
+            timerId = setTimeout(request, delay);
         }, delay)
     }
 }
