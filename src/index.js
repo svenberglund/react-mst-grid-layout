ff4f94b1b8318becffdea0899b8d4a12bbe40111
Sven Berglund <43970456+svenberglund@users.noreply.github.com>
2019-06-19 12:23:50 -0400

    Cleaning up old comments

--- src/index.js
+++ src/index.js
@@ -10,39 +10,8 @@ import {MstGridItem} from "./framework/models/mstGridItem";
 import {PubSubAPI} from "./framework/message-relay/pubSubAPI";
 
 /*
-// remodel like this:
-import echarts from 'echarts';
-import EchartsReactCore from './core';
-
-// export the Component the echarts Object.
-export default class EchartsReact extends EchartsReactCore {
-  constructor(props) {
-    super(props);
-    this.echartsLib = echarts;
-  }
-}
-
-
-export default class RMGL {
-    constructor(props) {
-      this.GridItem = GridItem;
-      this.MstGridLayout = MstGridLayout;
-
-      this.mstGrids = mstGrids;
-      this.MstGrid = MstGrid;
-      this.MstGridItem = MstGridItem;
-
-      this.GridElementSuper = GridElementSuper;
-
-      this.PubSubAPI = PubSubAPI;
-    }
-  }
-
+   Exposure of entrire API in object literal
 */
-
-// might do this insted, like PubSubAPI : export function PubSubAPI() {}
-
-//export default function MGL(){}
 
 var RMGL = {}
 RMGL.GridItem = GridItem;
