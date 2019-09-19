3d0cf63e8a6ce0d16db277c0d6a277556c72aa49
Sven Berglund <sven.3000.berglund@gmail.com>
2019-09-19 14:59:58 -0400

    (master) - Collapse function for grid items.

--- src/framework/components/gridItem.jsx
+++ src/framework/components/gridItem.jsx
@@ -1,4 +1,5 @@
 import _ from "lodash";
+import React from "react";
 import renderElmClasses from "../classStore";
 
 class GridItem {
@@ -12,7 +13,10 @@ class GridItem {
       var elmClass = renderElmClasses.get(ga.elementRenderClass);
       if (elmClass === null) elmClass = renderElmClasses.get("super"); // fallback on super class
 
-      return elmClass.renderElement(i, ga.subscriptionMap);
+      if (ga.show)
+        return elmClass.renderElement(i, ga.subscriptionMap);
+      else
+        return <span key={i} style={{ display: 'none' }} />
     });
   }
 }
